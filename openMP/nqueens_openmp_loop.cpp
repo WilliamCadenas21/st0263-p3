832e233b4a6743f6262f6e0d60d43e9778c584ec
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-10-12 09:51:35 -0500

    Serial code ready for openMP

--- openMP/nqueens_openmp_loop.cpp
+++ openMP/nqueens_openmp_loop.cpp
@@ -19,48 +19,54 @@ int main(int argc, char* argv[]){
 
 
 int solve_nqs(int N){
-    int solutions_counter = 0;
-    int actual_col = 0;
-    bool is_valid = false;
 
+    int solutions_counter = 0;
+
+    bool is_valid = false;
+    int actual_col = 1;
     vector<int> flatted_matrix(N,-1);
     // While true.
-    for(;;){
+    for (int iterator = 0; iterator < N; iterator++ ){
+        flatted_matrix[0] = iterator; 
+        actual_col = 1;
+        for(;;){
         // Stop condition -> End reached
-        if (actual_col < 0){
-            break;
-        }
-
-        // I found a solution due to I fill all columns
-        if (actual_col == N){
-            solutions_counter ++;
-            // Go back to the final col to continue finding solutions
-            actual_col --;
-        }
-        // Flag that indicates whethere we find a corret combination per column
-        is_valid = false;
-        
-        // Iterate over the row finding the position
-        // were the queen fit
-        for (int actual_pos = flatted_matrix[actual_col]; actual_pos < (N -1 ); actual_pos++){
-            flatted_matrix[actual_col] = actual_pos + 1;
-            // It's a valid position
-            // Advance to the next column
-            if ( check_position(flatted_matrix,actual_col) ){
-                is_valid= true;
+            if (actual_col == 0){
                 break;
             }
+
+            // I found a solution due to I fill all columns
+            if (actual_col == N){
+                solutions_counter ++;
+                // Go back to the final col to continue finding solutions
+                actual_col --;
+            }
+            // Flag that indicates whethere we find a corret combination per column
+            is_valid = false;
             
-        }
-        // If we find a valid position then move forward (Column)
-        if(is_valid){
-            actual_col ++;
-        }else{
-            // Backtracking. Go back
-            flatted_matrix[actual_col] = -1;
-            actual_col--;
+            // Iterate over the row finding the position
+            // were the queen fit
+            for (int actual_pos = flatted_matrix[actual_col]; actual_pos < (N -1 ); actual_pos++){
+                flatted_matrix[actual_col] = actual_pos + 1;
+                // It's a valid position
+                // Advance to the next column
+                if ( check_position(flatted_matrix,actual_col) ){
+                    is_valid= true;
+                    break;
+                }
+                
+            }
+            // If we find a valid position then move forward (Column)
+            if(is_valid){
+                actual_col ++;
+            }else{
+                // Backtracking. Go back
+                flatted_matrix[actual_col] = -1;
+                actual_col--;
+            }
         }
     }
+    
 
     return solutions_counter;
 }
