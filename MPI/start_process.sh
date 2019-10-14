26c874fa58c0a24094686c083c40dba35c3b1841
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-10-14 10:45:59 -0500

    Makefile and script

--- MPI/start_process.sh
+++ MPI/start_process.sh
@@ -1,5 +1,9 @@
+#Clean space
+rm -rf mpi_file.*
+rm NqueensMPI
+
 # Compile process
-mpiicpc -o -std=c++11 -qopenmp NqueensMPI nqueens_MPI_loop.cpp 
+mpiicpc  -std=c++11 -qopenmp -o NqueensMPI nqueens_MPI_loop.cpp 
 
 #Send work
 qsub mpi_file
