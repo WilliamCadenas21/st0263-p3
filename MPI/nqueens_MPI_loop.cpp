93b2e392b3eab9e4c927ad3383867354757d4527
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-10-14 18:24:20 -0500

    Adding debug process

--- MPI/nqueens_MPI_loop.cpp
+++ MPI/nqueens_MPI_loop.cpp
@@ -57,6 +57,7 @@ int main(int argc, char* argv[]){
             
             MPI_Send(&ranges[0], 2, MPI_INT, i, 0 ,MPI_COMM_WORLD);
         }
+        printf("All data have been seended ");
     }
 
 
@@ -65,18 +66,26 @@ int main(int argc, char* argv[]){
     // Get params
     MPI_Recv(&ranges_local[0], 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
+    printf("Proccess %d has %d - %d", rank,ranges_local[0],ranges_local[1])
+
     // Calculated solutions
     int solutions_number_local =  solve_nqs(N,ranges_local[0],ranges_local[1] );
     
     // send solutions tag = 1 -> Solutions tag, to master
+
+    printf("Procces %d has finished the job", rank);
+
     MPI_Send(&solutions_number_local, 1, MPI_INT, 0, 1 ,MPI_COMM_WORLD);
 
     if (rank == 0){
+        
         int global_sum_up = 0;
         int local_sum_up;
+        printf ("Master start receiving results");
         for(int i =0; i < size; i++){
             MPI_Recv(&local_sum_up, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
             global_sum_up = global_sum_up + local_sum_up;
+            printf ("Master received %d", rank);
         }
         cout << "Total combinations ->" << global_sum_up << endl;
     }
