5b616875fc9604ab8efadc015682bfb43f6c5105
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-10-14 20:32:52 -0500

    Add files via upload

---
+++ MPI/nqueens_MPI_V2.cpp
@@ -0,0 +1,196 @@
+
+#include <omp.h>
+#include "mpi.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int solve_nqs(int,int,int);
+bool check_position(vector<int>&,int);
+
+
+int main(int argc, char* argv[]){
+
+    MPI_Init (&argc, &argv); // Initialize MPI Env
+    int rank, size, namelen;
+    char name[MPI_MAX_PROCESSOR_NAME];
+    MPI_Status status;
+
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank); // ID of current process
+    MPI_Get_processor_name(name, &namelen); // Hostname of node
+    MPI_Comm_size (MPI_COMM_WORLD, &size); // Number of processes
+    printf("Hello World from rank %d runnung on %s! \n", rank, name);
+    if (rank == 0) printf("MPI Wold size = %d processes \n", size);
+
+    // Vector unit -> [0] Start position [1] end position
+    
+    vector<uint32_t> ranges_local;
+    // If i'm the master I'll send the ranges to my slaves
+    int N =stoi(argv[1]);
+
+    ranges_local.resize(2);
+
+    if (rank == 0) {
+
+        
+
+        
+        // The slop is the number of queens divided the number of cores.
+        int slop = (int) N/size;
+
+        // Defined the limits 
+        int left_limit = 0;
+        int right_limit = 0;
+
+        printf("The slop is %d, N is %d, The size is %d \n", slop, N, size);
+
+        for(int i =0; i < size; i++){
+            vector<uint32_t> ranges;
+
+            ranges.push_back(left_limit);
+
+            right_limit = slop * (i + 1);
+
+            if (i  == (size - 1)) right_limit = N;
+
+            ranges.push_back(right_limit);
+
+            left_limit = right_limit;
+
+            if (i == 0) {
+                ranges_local[0] = ranges[0];
+                ranges_local[1] = ranges[1];
+            }else {
+                MPI_Send(&ranges[0], 2, MPI_INT, i, 0 ,MPI_COMM_WORLD);
+            }
+            
+        }
+        printf("All data have been seended \n");
+    }
+
+
+    if (rank != 0){
+        MPI_Recv(&ranges_local[0], 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    }
+
+    // Get params
+
+    printf("Proccess %d has %d - %d \n", rank,ranges_local[0],ranges_local[1]);
+
+    // Calculated solutions
+    int solutions_number_local =  solve_nqs(N,ranges_local[0],ranges_local[1] );
+    
+    // send solutions tag = 1 -> Solutions tag, to master
+
+    printf("Procces %d has finished the job \n", rank);
+
+    if(rank != 0){
+        MPI_Send(&solutions_number_local, 1, MPI_INT, 0, 1 ,MPI_COMM_WORLD);
+    }
+    
+
+    if (rank == 0){
+        
+        int global_sum_up = solutions_number_local;
+        int local_sum_up;
+        printf ("Master start receiving results \n");
+        for(int i =1; i < size; i++){
+            MPI_Recv(&local_sum_up, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+            global_sum_up = global_sum_up + local_sum_up;
+            printf ("Master received %d \n", rank);
+        }
+        cout << "Total combinations ->" << global_sum_up << endl;
+    }
+    
+    MPI_Finalize(); //Terminate MPI Env
+
+    return 0;
+}
+
+
+int solve_nqs(int N, int lowerBound, int upperBound){
+
+    int solutions_counter_global = 0;
+
+    #pragma omp parallel
+    {
+        int solutions_counter_local = 0;
+        bool is_valid = false;
+        int actual_col = 1;
+        vector<int> flatted_matrix(N,-1);
+
+        #pragma omp for
+        for (int iterator = lowerBound; iterator < upperBound; iterator++ ){
+            flatted_matrix[0] = iterator; 
+            actual_col = 1;
+            for(;;){
+            // Stop condition -> End reached
+                if (actual_col == 0){
+                    break;
+                }
+
+                // I found a solution due to I fill all columns
+                if (actual_col == N){
+                    solutions_counter_local ++;
+                    // Go back to the final col to continue finding solutions
+                    actual_col --;
+                }
+                // Flag that indicates whethere we find a corret combination per column
+                is_valid = false;
+                
+                // Iterate over the row finding the position
+                // were the queen fit
+                for (int actual_pos = flatted_matrix[actual_col]; actual_pos < (N -1 ); actual_pos++){
+                    flatted_matrix[actual_col] = actual_pos + 1;
+                    // It's a valid position
+                    // Advance to the next column
+                    if ( check_position(flatted_matrix,actual_col) ){
+                        is_valid= true;
+                        break;
+                    }
+                    
+                }
+                // If we find a valid position then move forward (Column)
+                if(is_valid){
+                    actual_col ++;
+                }else{
+                    // Backtracking. Go back
+                    flatted_matrix[actual_col] = -1;
+                    actual_col--;
+                }
+            }
+        }
+
+        #pragma omp atomic
+            solutions_counter_global += solutions_counter_local;
+
+    }
+    return solutions_counter_global;
+}
+
+bool check_position(vector<int>& flatted_matrix, int actual_col){
+    int actual_pos = flatted_matrix[actual_col];
+    int upper_diagonal = actual_pos;
+    int lower_diagonal = actual_pos;
+    int center = actual_pos;
+
+    for (int column_to_check = actual_col - 1; column_to_check >= 0 ; column_to_check--){
+        upper_diagonal = upper_diagonal - 1;
+        lower_diagonal = lower_diagonal + 1;
+
+        if (flatted_matrix[column_to_check] == upper_diagonal){
+            return false;
+        }
+
+        if (flatted_matrix[column_to_check] == lower_diagonal){
+            return false;
+        }
+
+        if (flatted_matrix[column_to_check] == center){
+            return false;
+        }
+    }
+
+    return true;
+
+}
